f19da3aae30319108238daced0fa4255eb63b020
Alexander Panko <god@panki.ru>
2015-07-02 20:55:45 +0300

    Error handlers

--- web/errors.js
+++ web/errors.js
@@ -18,26 +18,19 @@ module.exports = function(app) {
 
     // error handlers
 
-    // development error handler
-    // will print stacktrace
-    if (app.get('env') === 'development') {
-
-        app.use(function(err, req, res, next) {
-            res.status(err.status || 500);
-            res.render('error', {
-                message: err.message,
-                error: err
-            });
-        });
-    }
-
     // production error handler
     // no stacktraces leaked to user
     app.use(function(err, req, res, next) {
-        res.status(err.status || 500);
-        res.render('error', {
-            message: err.message,
-            error: {}
-        });
+        
+        switch (err.code) {
+            case 'invalid':
+                res.status(422);
+                res.render('error', {error: err});
+                break;
+            default:
+                console.error(err.stack);
+                res.status(500);
+                res.render('error', {error: {message: 'Внутрення ошибка сервера'}});
+        }
     });
 };
