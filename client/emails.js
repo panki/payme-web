4d101748182e162c90aee6191d91d337e1a62398
Alexander Panko <god@panki.ru>
2015-07-03 19:37:30 +0300

    Fix links in emails

--- client/emails.js
+++ client/emails.js
@@ -5,17 +5,18 @@ function Emails(client) {
     this.client = client;
     this.config = client.config;
     var self = this;
-    
+
     this.get = function(emailId) {
         return this.client.get('/emails/' + emailId).then(function (email) {
+            var token = '?token=' + email.auth_token_id;
             email.template = email.type;
-            email.viewUrl = [self.config.baseUrl, 'emails/view', email.id].join('/');
-            email.confirmUrl = [self.config.baseUrl, 'invoice/confirm', email.invoice.id, '?auth_token=' + email.auth_token_id].join('/');
-            email.payUrl = [self.config.baseUrl, 'invoice/pay', email.invoice.id, '?auth_token=' + email.auth_token_id].join('/');
-            email.invoiceUrl = [self.config.baseUrl, 'invoice', email.invoice.id, '?auth_token=' + email.auth_token_id].join('/');
-            email.refuseUrl = [self.config.baseUrl, 'invoice/refuse', email.invoice.id, '?auth_token=' + email.auth_token_idd].join('/');
-            email.cancelUrl = [self.config.baseUrl, 'invoice/cancel', email.invoice.id, '?auth_token=' + email.auth_token_id].join('/');
-            email.termsUrl = [self.config.baseUrl, 'terms'].join('/');
+            email.viewUrl       = [self.config.baseUrl, 'emails/view', email.id].join('/');
+            email.invoiceUrl    = [self.config.baseUrl, 'invoice', email.invoice.id, token].join('/');
+            email.confirmUrl    = [self.config.baseUrl, 'invoice', email.invoice.id, 'send', token].join('/');
+            email.payUrl        = [self.config.baseUrl, 'invoice', email.invoice.id, 'pay',     token].join('/');
+            email.refuseUrl     = [self.config.baseUrl, 'invoice', email.invoice.id, 'refuse',  token].join('/');
+            email.cancelUrl     = [self.config.baseUrl, 'invoice', email.invoice.id, 'cancel',  token].join('/');
+            email.termsUrl      = [self.config.baseUrl, 'terms'].join('/');
             return email;
         });
     };
