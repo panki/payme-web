c5650e118927eb515cc9653692092d882b115239
JK <jk@bigdev.ru>
2015-07-06 16:16:21 +0300

    Added dev.paymepayme.com to config-dev.

--- config/config-dev.js
+++ config/config-dev.js
@@ -1,6 +1,6 @@
 var config = {
-    apiUrl: 'http://127.0.0.1:5000/api',
-    baseUrl: 'http://127.0.0.1:3000',
+    apiUrl: 'http://dev.paymepayme.com/api',    // No trailing slash.
+    baseUrl: 'http://dev.paymepayme.com',       // No trailing slash.
     redis: {
         host: '127.0.0.1',
         port: 6379,
