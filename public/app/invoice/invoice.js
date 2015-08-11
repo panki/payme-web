6edc63995fb9507642486bf0881c2c8662538568
Alexander Panko <god@panki.ru>
2015-08-11 21:20:29 +0300

    Final draft invoice form

--- public/app/invoice/invoice.js
+++ public/app/invoice/invoice.js
@@ -36,8 +36,7 @@
                     $scope.error = error;
                     return;
                 }
-                
-                alert(error.message);
+                $scope.error = error;
             };
 
             $scope.loadInvoice = function() {
