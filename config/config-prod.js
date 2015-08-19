533c22b00e7dd83c25deeea2001b6529365ea4f3
Alexander Panko <god@panki.ru>
2015-08-19 15:01:47 +0300

    Production config

---
+++ config/config-prod.js
@@ -0,0 +1,71 @@
+var config = {
+    apiUrl: 'http://payme4.ru/api',    // No trailing slash.
+    baseUrl: 'http://payme4.ru',       // No trailing slash.
+    redis: {
+        host: '127.0.0.1',
+        port: 6379,
+        db: 0
+    },
+    auth: {
+        deviceTtlMs: 10 * 365 * 24 * 3600 * 1000    // 10 years.
+    },
+    client: {
+        device: {
+            id: '63ef2d03-5251-409b-b037-0c22532c60c5',
+            userAgent: 'Payme web client'
+        }
+    },
+    mail: {
+        from: 'Payme Robot <robot@payme4.ru>',
+        transport: {
+            smtp: {
+                host: 'localhost',
+                port: 25,
+                secure: false,
+                ignoreTLS: true
+            }
+        }
+    },
+    invoices: {
+        minAmount: 100,
+        maxAmount: 75000
+    },
+    sentry: {
+        dns: ''
+    },
+    contacts: {
+        support: {
+            email: 'support@payme4.ru',
+            phone: '+371 67041100'
+        }
+    }
+};
+
+// Angular app config
+
+config.ng = {
+    apiUrl: config.apiUrl,
+    termUrl: config.baseUrl + '/transaction/confirmed/',
+    sessionPingInterval: 60000
+};
+
+// Mandrill config
+
+config.mandrill = {
+    hooks: {
+        events: {
+            url: config.baseUrl + '/mandrill/events',
+            key: '7JvQgzpTJP4p1NrfF48N8g'
+        },
+        inbound: {
+            url: config.baseUrl + '/mandrill/inbound',
+            key: 'qDUZwx3qlIcbxj3SNeSCEw',
+            relays: {
+                'admin@payme4.ru': 'admin@norvik.eu',
+                'support@payme4.ru': 'payme-support@norvik.eu'
+            }
+        }
+    }
+};
+    
+module.exports = config;
